0f28683fd60d8adef642330c06373f91666efe68
martmwangi <luthermwangi2@gmail.com>
2015-10-31 14:51:10 +0300

    Added scroll fire to the skills and interest section

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
 $(".button-collapse").sideNav();
+
+ var options = [
+    {selector: '#staggered-test', offset: 400, callback: 'Materialize.showStaggeredList("#staggered-test")' },
+  ];
+  Materialize.scrollFire(options);
