16b8e99cb171330823311a6698386d2d00d2881b
martmwangi <luthermwangi2@gmail.com>
2015-11-21 20:46:44 +0300

    Adding gallery section

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
+//Testing scroll spy effect
 $(".button-collapse").sideNav();
-
  var options = [
     {selector: '#staggered-test', offset: 400, callback: 'Materialize.showStaggeredList("#staggered-test")' },
   ];
   Materialize.scrollFire(options);
+//For the slider
